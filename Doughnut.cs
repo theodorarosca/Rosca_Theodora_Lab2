b53664fce6ee830a6ad8900a6d7fde334d3b34b0
Theodora Rosca <theodora_andreea15@outlook.com>
2021-11-14 22:35:53 +0200

    laborator 3

---
+++ Doughnut.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rosca_Theodora_Lab2
+{
+    class Doughnut
+    {
+        private DoughnutType mFlavor;
+        public DoughnutType Flavor
+        {
+            get
+            {
+                return mFlavor;
+            }
+            set
+            {
+                mFlavor = value;
+            }
+        }
+        private float mPrice;
+        public float Price
+        {
+            get
+            {
+                return mPrice;
+            }
+            set
+            {
+                mPrice = value;
+            }
+        }
+        public Doughnut(DoughnutType aFlavor)//constructor
+        {
+            mFlavor = aFlavor;
+        }
+    }
+    public enum DoughnutType
+    {
+        Glazed,
+        Sugar,
+        Lemon,
+        Chocolate,
+        Vanilla
+    }
+    
+}
